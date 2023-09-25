754189435e63ceebdb9370ea4c4c0acf5a959669
theaskou <thsk0001@stud.kea.dk>
2023-09-25 15:47:35 +0200

    initial commit

---
+++ src/main/java/Room.java
@@ -0,0 +1,5 @@
+public class Room {
+    private String name;
+    private String beskrivelse;
+    // Skal klassen indeholde et ArrayList til at håndtere hvilke retninger man kan bevæge sig i fra det pågældende rum?
+}
---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class UserInterface {
+
+    // Opgave 1: Start med at lave brugerfladen + kommandoer i switch-case, med String håndtering
+    public void startGame(){
+        System.out.println("Welcome to adventure game!");
+        System.out.println("You are now standing i room 1. Would you like to go east or go south?");
+        Scanner keyboard = new Scanner(System.in);
+
+
+        // 1. Current position:
+        // Room objekt, med navn og beskrivelse.
+
+
+        // 2. Udskrive hvilke retninger man kan bevæge sig i fra current position/Room object.
+
+        // 3. Metode til at flytte sig fra ét rum til et andet (set):
+
+
+        // 4. Gentage processen
+
+
+
+
+    }
+}
